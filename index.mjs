f8eeab23796b9737cee33a8caf49fe9bd2c5cf79
Sahad <msahad124@gmail.com>
2023-04-11 19:15:30 +0530

    added transporter

--- index.mjs
+++ index.mjs
@@ -1,9 +1,37 @@
 import http from 'http'
+import nodemailer from 'nodemailer'
 
 const port = 5000
 
 const server = http.createServer((req, res)=>{
     res.end("Hello world")
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+          clientId: process.env.OAUTH_CLIENTID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        }
+      });
+
+      let mailOptions = {
+        from: 'msahad124@gmail.com',
+        to: 'msahad546@gmail.com',
+        subject: 'Nodemailer Project',
+        text: 'Hi from your nodemailer project'
+      };
+
+      transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+          console.log("Error " + err);
+        } else {
+          console.log("Email sent successfully");
+        }
+      });
 })
 
 server.listen(port, ()=>{
